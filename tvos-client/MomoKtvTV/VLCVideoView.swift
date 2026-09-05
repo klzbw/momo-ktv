1da4ff792d291ffc7ddfeb23fb46516f71f7ba98
klzbw <klzbw@163.com>
2026-09-05 17:20:02 +0800

    fix(tvos): 重写VLCVideoView，使用activeDrawable管理视频输出

--- tvos-client/MomoKtvTV/VLCVideoView.swift
+++ tvos-client/MomoKtvTV/VLCVideoView.swift
@@ -5,6 +5,7 @@ import TVVLCKit
 #endif
 
 /// VLC视频输出视图 - 用于播放MKV等AVFoundation不支持的格式
+/// 使用activeDrawable管理，确保只有当前可见的视图输出视频
 struct VLCVideoView: UIViewRepresentable {
     let vlcManager: VLCPlayerManager
 
@@ -13,15 +14,20 @@ struct VLCVideoView: UIViewRepresentable {
         view.backgroundColor = .black
         view.clipsToBounds = true
         vlcManager.addDrawable(view)
+        // 设置为活动drawable（这个视图出现时接管视频输出）
+        DispatchQueue.main.async {
+            vlcManager.setActiveDrawable(view)
+        }
         return view
     }
 
     func updateUIView(_ uiView: UIView, context: Context) {
-        // 每次更新时重新设置 drawable，确保视频输出正确
-        vlcManager.addDrawable(uiView)
+        // 视图更新时确保它是活动drawable
+        vlcManager.setActiveDrawable(uiView)
     }
 
-    func dismantleUIView(_ uiView: UIView, coordinator: ()) {
-        vlcManager.removeDrawable(uiView)
+    static func dismantleUIView(_ uiView: UIView, coordinator: ()) {
+        // 清除活动drawable，会自动切换到下一个可用的视图
+        VLCPlayerManager.shared.clearActiveDrawable(uiView)
     }
 }
